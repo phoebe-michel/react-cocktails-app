00fb674ffb32bab574621081700cd0c55ee04f14
Phoebe Michel <awkwardblackcoder@gmail.com>
2024-05-30 18:37:28 -0400

    fix: move code logic for search bar

--- cocktails-app/src/pages/HomePage.jsx
+++ cocktails-app/src/pages/HomePage.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import Hero from "../components/Hero";
-import Cocktails from "../components/Cocktails";
+import HeaderImages from "../components/HeaderImages";
+import { FiSearch } from "react-icons/fi";
 
 const HomePage = () => {
   return (
     <main className="container-xl h-screen">
       <Hero />
-      <Cocktails />
-      <div></div>
+
+      <div className="relative w-full">
+        <HeaderImages />
+
+        {/* Search Bar */}
+        <div className="search-bar flex justify-center">
+          <div className="absolute bottom-0.5 w-4/6 pl-3">
+            <span className="absolute inset-y-0 left-5 text-gray-500 flex items-center pl-10">
+              <FiSearch size={18} />
+            </span>
+            <input
+              className="placeholder:text-gray-500 block w-full border rounded-md py-5 pl-20 pr-3 shadow-xl focus:outline-none  focus:ring-gray-400 focus:ring-1 sm:text-lg"
+              type="text"
+              name="search"
+              placeholder="Enter a cocktail name..."
+            />
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
