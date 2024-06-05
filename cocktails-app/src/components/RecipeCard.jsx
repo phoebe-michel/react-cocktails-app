c8347995666d0c2f542446ab0adea19ed7e7b779
Phoebe Michel <awkwardblackcoder@gmail.com>
2024-06-05 13:00:26 -0400

    fix: add responsiveness to recipecard component

--- cocktails-app/src/components/RecipeCard.jsx
+++ cocktails-app/src/components/RecipeCard.jsx
@@ -38,20 +38,29 @@ const RecipeCard = () => {
   }
 
   return (
-    <section className="container mx-auto px-44 gap-24 py-28 w-full grid grid-cols-2 justify-center">
-      <img className="" src={drink.strDrinkThumb} alt={drink.strDrink} />
+    <section className="container mx-auto py-8 xl:py-16 md:w-5/6 w-full">
       <section className="recipe place-self-center max-h-screen">
-        <div>
-          <h4 className="text-lg highlight font-bold">Featured Cocktail</h4>
-          <h1 className="text-5xl font-semibold font-serif">
-            {drink.strDrink}
-          </h1>
-          <h3 className="text-lg text-zinc-400 font-light">
-            {drink.strCategory}
-          </h3>
-          <div className="overflow-y-scroll max-h-[300px] py-5">
-            <div className="ingredients py-5">
-              <h3 className="font-medium text-2xl text-zinc-800 pb-3">
+        <div className="grid auto-rows-min lg:grid-cols-2 px-8 space-y-4 md:space-y-9 lg:space-y-0 lg:py-16 lg:gap-x-10">
+          <img
+            className="row-start-2 lg:row-start-1 lg:row-span-5"
+            src={drink.strDrinkThumb}
+            alt={drink.strDrink}
+          />
+          <div className="heading row-start-1 lg:col-start-2 lg:block space-y-1 pb-7 lg:pb-1 lg:pt-10">
+            {" "}
+            <h4 className="text-lg md:text-2xl highlight font-bold">
+              Featured Cocktail
+            </h4>
+            <h1 className="text-4xl md:text-6xl lg:text-5xl font-semibold font-serif">
+              {drink.strDrink}
+            </h1>
+            <h3 className="text-lg md:text-2xl text-zinc-400 font-light">
+              {drink.strCategory}
+            </h3>
+          </div>
+          <div className="row-start-3 md:py-5 lg:row-span-2 lg:col-start-2 lg:overflow-y-scroll lg:max-h-[300px]">
+            <div className="ingredients lg:text-lg">
+              <h3 className="font-medium text-2xl xl:text-3xl text-zinc-800 pb-3">
                 Ingredients
               </h3>
               {ingredients.map((ingredient, index) => (
@@ -60,14 +69,14 @@ const RecipeCard = () => {
                 </li>
               ))}
             </div>
-            <div className="instructions py-5">
-              <h3 className="font-medium text-2xl text-zinc-800 pb-3">
+            <div className="instructions py-5 lg:text-lg">
+              <h3 className="font-medium text-2xl xl:text-3xl text-zinc-800 pb-3">
                 Instructions
               </h3>
               {drink.strInstructions}
             </div>
           </div>
-          <div className="py-10">
+          <div className="row-start-5 lg:col-start-2 lg:pb-10">
             <button
               onClick={fetchData}
               className="bg-[#ff0033] text-white shadow-md rounded-lg text-lg px-5 py-2 cursor-pointer"
