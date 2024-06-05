cd33daa13b97a25ac9bf9b1c5c25e010165b1b28
Phoebe Michel <awkwardblackcoder@gmail.com>
2024-06-04 20:46:22 -0400

    add responsive breakpoints to navbar

--- cocktails-app/src/components/Nav.jsx
+++ cocktails-app/src/components/Nav.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { FiMenu } from "react-icons/fi";
 
 const Nav = () => {
   return (
-    <nav className="fixed top-0 z-10 w-screen bg-white">
-      <div className="container mx-auto">
-        <div className="flex justify-between items-center text-lg">
-          <div className="logo py-5 text-2xl font-semibold cursor-pointer">
-            C&C
+    <nav className="fixed top-0 z-10 w-screen bg-white ">
+      <div className="lg:container mx-auto">
+        <div className="flex justify-between items-center p-6 md: px-8 lg:px-10 md:py-3 lg:py-8">
+          <div className="logo md:text-2xl font-semibold font-serif cursor-pointer">
+            Cocktails with Code
           </div>
-          <ul className="flex">
+          <FiMenu className="size-5 md:hidden" />
+
+          <ul className="hidden md:flex md:text-lg lg:text-xl">
             <li className="p-5 hover:border-b-[#ff0033] cursor-pointer">
               Home
             </li>
