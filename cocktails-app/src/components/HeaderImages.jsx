69d395167c9af1b60fa6e9c83665cbe4df2402ae
Phoebe Michel <awkwardblackcoder@gmail.com>
2024-06-05 14:42:31 -0400

    fix: show/hide images in HeaderImages based on screen size; remove commented out code

--- cocktails-app/src/components/HeaderImages.jsx
+++ cocktails-app/src/components/HeaderImages.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const HeaderImages = () => {
   return (
-    <div className="cocktail-images h-auto">
-      <div className="container mx-auto pb-6">
+    <div className="cocktail-images h-auto ">
+      <div className="container mx-auto pb-6 lg:px-16">
         {/* <h1 className="text-slate-200 text-6xl py-8 text-center">
           Featured Cocktails
         </h1> */}
@@ -29,11 +29,6 @@ const HeaderImages = () => {
             alt="Spiced Peach Punch | 13032"
           />
         </div>
-        {/* <div className="pt-20 pb-4 flex justify-center">
-          <button className="border-2 shadow-lg shadow-zinc-800 border-slate-200 hover:border-none hover:bg-[#ff0033] text-slate-200 font-semibold rounded-full text-xl px-8 py-2 cursor-pointer">
-            View More
-          </button>
-        </div> */}
       </div>
     </div>
   );
