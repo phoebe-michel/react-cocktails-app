a626addf5f6d112107528be99829e4719744d43e
Phoebe Michel <awkwardblackcoder@gmail.com>
2024-06-11 21:03:51 -0400

    feat: add cocktails route for search results

--- cocktails-app/src/App.jsx
+++ cocktails-app/src/App.jsx
@@ -6,13 +6,23 @@ import {
 } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout";
 import NotFoundPage from "./pages/NotFound";
+import NoResultsFoundPage from "./pages/NoResultsFoundPage";
 import HomePage from "./pages/HomePage";
+import SearchResultsPage, {
+  searchResultsLoader,
+} from "./pages/SearchResultsPage";
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<RootLayout />}>
         <Route index element={<HomePage />} />
+        <Route
+          path="/cocktails/:cocktailName"
+          element={<SearchResultsPage />}
+          errorElement={<NoResultsFoundPage />}
+          loader={searchResultsLoader}
+        />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     )
