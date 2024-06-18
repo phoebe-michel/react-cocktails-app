af727b0f74bdc0035317dcfc20fb27687ab33c5e
Phoebe Michel <awkwardblackcoder@gmail.com>
2024-06-18 16:58:01 -0400

    feat:  add scrolleft and scroll right buttons to categories bar

--- cocktails-app/src/components/Categories.jsx
+++ cocktails-app/src/components/Categories.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import { React, useRef } from "react";
 import { useState, useEffect } from "react";
 import DrinkCard from "./DrinkCard";
 import { useNavigate, Link } from "react-router-dom";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
 const Cocktails = ({ isHome = false }) => {
   const [categories, setCategories] = useState([]);
   const [cocktails, setCocktails] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("Ordinary Drink");
+  const categoriesRef = useRef(null);
   const navigate = useNavigate();
 
   const fetchCategories = async () => {
@@ -33,7 +35,7 @@ const Cocktails = ({ isHome = false }) => {
     }
   };
 
-  const handleClick = (category) => {
+  const handleCategoryClick = (category) => {
     setSelectedCategory(category);
     fetchDrinksbyCategory(category);
   };
@@ -43,6 +45,14 @@ const Cocktails = ({ isHome = false }) => {
     fetchDrinksbyCategory(selectedCategory);
   }, []);
 
+  const scrollLeft = () => {
+    categoriesRef.current.scrollBy({ left: -220, behavior: "smooth" });
+  };
+
+  const scrollRight = () => {
+    categoriesRef.current.scrollBy({ left: 220, behavior: "smooth" });
+  };
+
   return (
     <section className="cocktails-section container mx-auto min-h-[100lvh] h-auto flex items-center py-10 md:py-0 lg:px-10">
       <div>
@@ -51,31 +61,48 @@ const Cocktails = ({ isHome = false }) => {
             <h2 className="text-4xl md:text-5xl text-center font-bold text-slate-700">
               Drinks by Category
             </h2>
-            <div className="flex justify-center items-center pt-5">
-              <ul className="categories font-medium text-lg lg:text-xl flex flex-wrap px-8 rounded-full text-slate-700 border-2 border-slate-300 shadow-md">
-                {categories.map((category, index) => {
-                  return (
-                    <li
-                      key={index}
-                      onClick={() => handleClick(category)}
-                      className={
-                        "cursor-pointer w-auto px-8 py-3 hover:text-white  " +
-                        (selectedCategory === category
-                          ? "bg-[#ff0033] text-white hover:bg-[#ff0033]"
-                          : "hover:bg-[#ff0033]/[0.5]")
-                      }
-                    >
-                      {category}
-                      {/* <div className="border-2 w-6"></div> */}
-                    </li>
-                  );
-                })}
-              </ul>
+            <div className="flex justify-center items-center pt-8 px-32">
+              <div className="flex max-w-full">
+                <button
+                  onClick={scrollLeft}
+                  className="rounded-l-full border-l-2 px-6 shadow-md bg-[#ff0033] text-white"
+                >
+                  <FaAngleLeft size={20} />
+                </button>
+                <ul
+                  ref={categoriesRef}
+                  className="categories font-medium text-lg lg:text-xl flex flex-nowrap overflow-hidden text-slate-700 border-2 border-slate-300 shadow-md"
+                >
+                  {categories.map((category, index) => {
+                    return (
+                      <li
+                        key={index}
+                        onClick={() => handleCategoryClick(category)}
+                        className={
+                          "cursor-pointer border-x-2 border-gray-200 py-5 px-5 text-nowrap hover:text-white min-w-[225px] w-auto flex items-center justify-center " +
+                          (selectedCategory === category
+                            ? "bg-[#ff0033] text-white hover:bg-[#ff0033]"
+                            : "hover:bg-[#ff0033]/[0.5]")
+                        }
+                      >
+                        {category}
+                        {/* <div className="border-2 w-6"></div> */}
+                      </li>
+                    );
+                  })}
+                </ul>
+                <button
+                  onClick={scrollRight}
+                  className="rounded-r-full border-r-2 px-6 shadow-md bg-[#ff0033] text-white"
+                >
+                  <FaAngleRight size={20} />
+                </button>
+              </div>
             </div>
           </div>
         </div>
 
-        <div className="cocktails px-10 xl:px-0 lg:py-12 grid justify-items-center grid-cols-1 md:grid-cols-2 xl:grid-cols-5 gap-10 mx-auto">
+        <div className="container cocktails px-10 xl:px-0 lg:py-12 grid justify-items-center grid-cols-1 md:grid-cols-2 xl:grid-cols-5 gap-6 mx-auto">
           {isHome
             ? cocktails.slice(0, 5).map((drink, index) => {
                 return (
